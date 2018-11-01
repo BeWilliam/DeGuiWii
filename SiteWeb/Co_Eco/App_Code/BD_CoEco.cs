a792c0ede5697e42cfd75e3035cc116d6802b3c9
Guillaume Gagnon <guillaume-gagnon@live.ca>
2018-10-31 22:27:38 -0400

    No fucking idea

---
+++ SiteWeb/Co_Eco/App_Code/BD_CoEco.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Linq;
+
+public class BD_CoEco
+{
+    public static List<T_Employe> GetListeEmploye()
+    {
+        CoEco_BDDataContext BD = new CoEco_BDDataContext();
+        Table<T_Employe> tableEmp = BD.T_Employe;
+        List<T_Employe> listeEmp = tableEmp.ToList();
+
+        return listeEmp;
+    }
+}
