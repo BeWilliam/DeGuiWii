d7cec235822f72a8e037790c24f235ed97a79120
Guillaume Gagnon <guillaume-gagnon@live.ca>
2018-11-12 16:11:31 -0500

    Connexion

--- Site_Web_V2/SiteWeb/Co_Eco/Menu.aspx.cs
+++ Site_Web_V2/SiteWeb/Co_Eco/Menu.aspx.cs
@@ -9,6 +9,9 @@ public partial class Menu : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        if (Session["username"] == null || Session["username"] == "")
+        {
+            Response.Redirect("index.aspx");
+        }
     }
 }
