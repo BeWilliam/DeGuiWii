53bd222a4ea94a3e996d3e2d0f961478aeb03349
Denis Theriault <denis.theriault53@gmail.com>
2018-11-29 15:40:45 -0500

    banqueHrs

--- Site_Web_V2/SiteWeb/Co_Eco/AjoutBanqueHeure.aspx
+++ Site_Web_V2/SiteWeb/Co_Eco/AjoutBanqueHeure.aspx
@@ -3,6 +3,9 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="cph_title" runat="Server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="cph_left" runat="Server">
+    
+    <asp:Button ID="btn_ajouter" runat="server" Text="Ajouter" Visible="true" OnClick="btn_ajouter_Click" CssClass="btn btn-success" style="margin-top: 10px; width: 100%; color: #000000; margin-left: 5px;" />
+    <asp:Button ID="btn_annuler" runat="server" Text="Annuler"  Visible="true" OnClick="btn_annuler_Click" CssClass="btn btn-danger" style="margin-top: 10px; width: 100%; color: #000000; margin-left: 5px;" />
 </asp:Content>
 <asp:Content ID="Content3" ContentPlaceHolderID="cph_contenu" runat="Server">
 
@@ -30,6 +33,8 @@
         </div>
 
     </div>
+
+
 
 </asp:Content>
 
--- Site_Web_V2/SiteWeb/Co_Eco/AjoutBanqueHeure.aspx.cs
+++ Site_Web_V2/SiteWeb/Co_Eco/AjoutBanqueHeure.aspx.cs
@@ -7,8 +7,86 @@ using System.Web.UI.WebControls;
 
 public partial class AjoutBanqueHeure : System.Web.UI.Page
 {
+    string idEmp;
+    T_Employe emp;
     protected void Page_Load(object sender, EventArgs e)
     {
 
+        idEmp = Request.QueryString["id"];
+        emp = BD_CoEco.GetEmpByID(int.Parse(idEmp));
+
+        if (emp.congesFeries!=null)
+        {
+            tb_congesFeries.Text = emp.congesFeries.ToString();
+        }
+        if (emp.congesMaladie != null)
+        {
+            tb_congesMaladie.Text = emp.congesMaladie.ToString();
+        }
+        if (emp.congesPersonnels != null)
+        {
+            tb_congesPersonnels.Text = emp.congesPersonnels.ToString();
+        }
+        if (emp.heuresAccumuleesOuSansSolde != null)
+        {
+            tb_heuresAccumuleesOuSansSolde.Text = emp.heuresAccumuleesOuSansSolde.ToString();
+        }
+        if (emp.vacances != null)
+        {
+            tb_vacances.Text = emp.vacances.ToString();
+        }
+
+    }
+
+    protected void btn_ajouter_Click(object sender, EventArgs e)
+    {
+        if (tb_congesFeries.Text != "")
+        {
+            emp.congesFeries = float.Parse(tb_congesFeries.Text);
+        }
+        else
+        {
+            emp.congesFeries = 0;
+        }
+        if (tb_congesMaladie.Text != "")
+        {
+            emp.congesMaladie = float.Parse(tb_congesMaladie.Text);
+        }
+        else
+        {
+            emp.congesMaladie = 0;
+        }
+        if (tb_congesPersonnels.Text != "")
+        {
+            emp.congesPersonnels = float.Parse(tb_congesPersonnels.Text);
+        }
+        else
+        {
+            emp.congesPersonnels = 0;
+        }
+        if (tb_heuresAccumuleesOuSansSolde.Text != "")
+        {
+            emp.heuresAccumuleesOuSansSolde = float.Parse(tb_heuresAccumuleesOuSansSolde.Text);
+        }
+        else
+        {
+            emp.heuresAccumuleesOuSansSolde = 0;
+        }
+        if (tb_vacances.Text != "")
+        {
+            emp.vacances = float.Parse(tb_vacances.Text);
+        }
+        else
+        {
+            emp.vacances = 0;
+        }
+
+        BD_CoEco.UpdateEmploye(emp);
+        Response.Redirect("BanqueHeureAdmin.aspx");
+    }
+
+    protected void btn_annuler_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("BanqueHeureAdmin.aspx");
     }
 }
