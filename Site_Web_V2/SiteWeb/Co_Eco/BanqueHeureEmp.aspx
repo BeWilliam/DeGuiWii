66c98af23342e06f1b53eb8573ad72f0ae085281
Denis Theriault <denis.theriault53@gmail.com>
2018-12-01 16:30:04 -0500

    banqueHeuresEmp

--- Site_Web_V2/SiteWeb/Co_Eco/BanqueHeureEmp.aspx
+++ Site_Web_V2/SiteWeb/Co_Eco/BanqueHeureEmp.aspx
@@ -1,92 +1,18 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="BanqueHeureEmp.aspx.cs" Inherits="BanqueHeureEmp" %>
 
-<asp:Content ID="Content1" ContentPlaceHolderID="cph_title" Runat="Server">
+<asp:Content ID="Content1" ContentPlaceHolderID="cph_title" runat="Server">
 </asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="cph_left" Runat="Server">
+<asp:Content ID="Content2" ContentPlaceHolderID="cph_left" runat="Server">
 </asp:Content>
-<asp:Content ID="Content3" ContentPlaceHolderID="cph_contenu" Runat="Server">
+<asp:Content ID="Content3" ContentPlaceHolderID="cph_contenu" runat="Server">
 
-    <div class="form-row">
-        <div class="col-md-1">
-            <label for="validationTooltip01">Congés</label>
-            </div>
-        <div class="col-md-2">
-            <label for="validationTooltip01">Congés Pris</label>
-            </div>
-        <div class="col-md-2">
-            <label for="validationTooltip01">Congés total</label>
-            </div>
-        </div>
-            <div class="form-row">
-        <div class="col-md-1">
-            <label for="validationTooltip01">Congés fériés</label>
-            </div>
-            <div class="col-md-2">
-            <asp:TextBox ID="tb_congesFeriesPris" runat="server" MaxLength="4" CssClass="col-sm-4"></asp:TextBox>
-            </div>
-            <div class="col-md-2">
-            <asp:TextBox ID="tb_congesFeriesTotal" runat="server"></asp:TextBox>
-
-            <div class="valid-tooltip">
-            </div>
+    <div class="col-md-6 offset-md-3">
+        <div class="col-md-auto" style="text-align: center;">
+            <h2>Banque d'heures</h2>
         </div>
     </div>
 
-    <div class="form-row">
-        <div class="col-md-1">
-            <label for="validationTooltip01">Congés Maladie</label>
-            </div>
-            <div class="col-md-2">
-            <asp:TextBox ID="tb_congeMaladiePris" runat="server" MaxLength="4"  CssClass="col-sm-4"></asp:TextBox>
-            </div>
-            <div class="col-md-2">
-            <asp:TextBox ID="tb_congeMaladieTotal" runat="server"></asp:TextBox>
-            <div class="valid-tooltip">
-            </div>
-        </div>
-    </div>
-
-        <div class="form-row">
-        <div class="col-md-1">
-            <label for="validationTooltip01">Congés Personnel</label>
-            </div>
-            <div class="col-md-2">
-            <asp:TextBox ID="tb_congesPersonnelsPris" runat="server" MaxLength="4" CssClass="col-sm-4"></asp:TextBox>
-            </div>
-            <div class="col-md-2">
-            <asp:TextBox ID="tb_congesPersonnelsTotal" runat="server"></asp:TextBox>
-            <div class="valid-tooltip">
-            </div>
-        </div>
-    </div>
-
-            <div class="form-row">
-        <div class="col-md-1">
-            <label for="validationTooltip01">Heures accumulées ou sans solde</label>
-            </div>
-            <div class="col-md-2">
-            <asp:TextBox ID="tb_hrsAccPris" runat="server" MaxLength="4" CssClass="col-sm-4"></asp:TextBox>
-            </div>
-            <div class="col-md-2">
-            <asp:TextBox ID="tb_hrsAccTotal" runat="server"></asp:TextBox>
-            <div class="valid-tooltip">
-            </div>
-        </div>
-    </div>
-
-                <div class="form-row">
-        <div class="col-md-1">
-            <label for="validationTooltip01">Vacances</label>
-            </div>
-            <div class="col-md-2">
-            <asp:TextBox ID="tb_vacancesPris" runat="server" MaxLength="4" CssClass="col-sm-4"></asp:TextBox>
-            </div>
-            <div class="col-md-2">
-            <asp:TextBox ID="tb_vacancesTotal" runat="server"></asp:TextBox>
-            <div class="valid-tooltip">
-            </div>
-        </div>
-    </div>
+        <asp:Table runat="server" ID="Tableau_BanqueDheures" CssClass="table" />
 
 </asp:Content>
 
--- Site_Web_V2/SiteWeb/Co_Eco/BanqueHeureEmp.aspx.cs
+++ Site_Web_V2/SiteWeb/Co_Eco/BanqueHeureEmp.aspx.cs
@@ -10,33 +10,14 @@ public partial class BanqueHeureEmp : System.Web.UI.Page
     int idEmp;
     protected void Page_Load(object sender, EventArgs e)
     {
+        HeaderTableau();
         if (!IsPostBack)
         {
             idEmp = int.Parse(Session["idEmp"].ToString());
         }
 
         T_Employe emp = BD_CoEco.GetEmpByID(idEmp);
-
-        if (emp.congesFeries != null)
-        {
-            tb_congesFeriesTotal.Text = emp.congesFeries.ToString();
-        }
-        if (emp.congesMaladie != null)
-        {
-            tb_congeMaladieTotal.Text = emp.congesMaladie.ToString();
-        }
-        if (emp.congesPersonnels != null)
-        {
-            tb_congesPersonnelsTotal.Text = emp.congesPersonnels.ToString();
-        }
-        if (emp.heuresAccumuleesOuSansSolde != null)
-        {
-            tb_hrsAccTotal.Text = emp.heuresAccumuleesOuSansSolde.ToString();
-        }
-        if (emp.vacances != null)
-        {
-            tb_vacancesTotal.Text = emp.vacances.ToString();
-        }
+        float nbHeures = 0;
 
         float maladie = 0;
         float feries = 0;
@@ -49,7 +30,7 @@ public partial class BanqueHeureEmp : System.Web.UI.Page
         {
             if (fdt.idEmp == idEmp)
             {
-                if(fdt.idCategorie == 191)
+                if (fdt.idCategorie == 191)
                 {
                     maladie += Utilitaires.GetHeureFDT(fdt.idFeuilleDeTemps);
                 }
@@ -64,21 +45,132 @@ public partial class BanqueHeureEmp : System.Web.UI.Page
                 }
                 else if (fdt.idCategorie == 194)
                 {
-                    hrsAcc+= Utilitaires.GetHeureFDT(fdt.idFeuilleDeTemps);
+                    hrsAcc += Utilitaires.GetHeureFDT(fdt.idFeuilleDeTemps);
                 }
                 else if (fdt.idCategorie == 255)
                 {
-                    personnel+=Utilitaires.GetHeureFDT(fdt.idFeuilleDeTemps);
+                    personnel += Utilitaires.GetHeureFDT(fdt.idFeuilleDeTemps);
                 }
             }
         }
-        tb_congeMaladiePris.Text = maladie.ToString();
-        tb_congesFeriesPris.Text = feries.ToString();
-        tb_vacancesPris.Text = vacances.ToString();
-        tb_hrsAccPris.Text = hrsAcc.ToString();
-        tb_congesPersonnelsPris.Text = personnel.ToString();
+
+        if (emp.congesFeries != null)
+        {
+            ShowEmp(feries,(float)emp.congesFeries,"Heures congées fériés");
+            nbHeures += (float)emp.congesFeries;
+        }
+        else
+        {
+            ShowEmp(feries, 0, "Heures congées fériés");
+        }
+        if (emp.congesMaladie != null)
+        {
+            ShowEmp(maladie, (float)emp.congesMaladie, "Heures congées maladie");
+            nbHeures += (float)emp.congesMaladie;
+        }
+        else
+        {
+            ShowEmp(maladie, 0, "Heures congées maladie");
+        }
+        if (emp.congesPersonnels != null)
+        {
+            ShowEmp(personnel, (float)emp.congesPersonnels, "Heures congées Personnels");
+            nbHeures += (float)emp.congesPersonnels;
+        }
+        else
+        {
+            ShowEmp(personnel, 0, "Heures congées Personnels");
+        }
+        if (emp.heuresAccumuleesOuSansSolde != null)
+        {
+            ShowEmp(hrsAcc, (float)emp.heuresAccumuleesOuSansSolde, "Heures accumulées ou sans solde");
+            nbHeures += (float)emp.heuresAccumuleesOuSansSolde;
+        }
+        else
+        {
+            ShowEmp(hrsAcc, 0, "Heures accumulées ou sans solde");
+        }
+        if (emp.vacances != null)
+        {
+            ShowEmp(hrsAcc, (float)emp.vacances, "Heures vacances");
+            nbHeures += (float)emp.vacances;
+        }
+        else
+        {
+            ShowEmp(hrsAcc, 0, "Heures vacances");
+        }
+
+        TableRow tr = new TableRow();
+        TableCell tc_nomConge = new TableCell();
+
+        tc_nomConge.Text = "Total";
+
+        TableCell tc_HrsPrises = new TableCell();
+        tc_HrsPrises.Text = nbHeures.ToString();
+
+
+        TableCell tc_HrsTotal = new TableCell();
+        float HrsAuTotal = (maladie + feries + vacances + hrsAcc + personnel);
+        tc_HrsTotal.Text = HrsAuTotal.ToString();
+
+        TableCell tc_HrsRestantes = new TableCell();
+        tc_HrsRestantes.Text = (HrsAuTotal - nbHeures).ToString();
+
+        tr.Cells.Add(tc_nomConge);
+        tr.Cells.Add(tc_HrsPrises);
+        tr.Cells.Add(tc_HrsTotal);
+        tr.Cells.Add(tc_HrsRestantes);
+        Tableau_BanqueDheures.Rows.Add(tr);
+
+    }
+    void HeaderTableau()
+    {
+        TableHeaderRow thr = new TableHeaderRow();
+
+        TableHeaderCell thc_Conges = new TableHeaderCell();
+        thc_Conges.Text = "Congés";
+        thc_Conges.Width = new Unit("25%");
+        TableHeaderCell thc_HrsPrises = new TableHeaderCell();
+        thc_HrsPrises.Text = "Heures Prises";
+        thc_HrsPrises.Width = new Unit("25%");
+        TableHeaderCell thc_HrsTotal = new TableHeaderCell();
+        thc_HrsTotal.Text = "Heures au total";
+        thc_HrsTotal.Width = new Unit("25%");
+        TableHeaderCell thc_HrsRestantes = new TableHeaderCell();
+        thc_HrsRestantes.Text = "Heures restantes";
+        thc_HrsRestantes.Width = new Unit("25%");
+
+        thr.Cells.Add(thc_Conges);
+        thr.Cells.Add(thc_HrsPrises);
+        thr.Cells.Add(thc_HrsTotal);
+        thr.Cells.Add(thc_HrsRestantes);
+        thr.ID = "thr_ID";
+        Tableau_BanqueDheures.Rows.Add(thr);
+    }
+    private void ShowEmp(float p_congesPris, float p_total, string congeRectifie)
+    {
+        TableRow tr = new TableRow();
+        TableCell tc_nomConge = new TableCell();
+
+        tc_nomConge.Text = congeRectifie;
+        
+        TableCell tc_HrsPrises = new TableCell();
+        tc_HrsPrises.Text = p_congesPris.ToString();
+
+
+        TableCell tc_HrsTotal = new TableCell();
+        tc_HrsTotal.Text = p_total.ToString();
+
+
+        TableCell tc_HrsRestantes = new TableCell();
+        tc_HrsRestantes.Text = (p_total - p_congesPris).ToString();
+
+        tr.Cells.Add(tc_nomConge);
+        tr.Cells.Add(tc_HrsPrises);
+        tr.Cells.Add(tc_HrsTotal);
+        tr.Cells.Add(tc_HrsRestantes);
+        Tableau_BanqueDheures.Rows.Add(tr);
 
     }
 
-    
 }
